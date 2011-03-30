41a263117b5deef204469c38439f99489924db4c
Samuel Lampa <samuel.lampa@rilnet.com>
2011-03-30 19:31:51 +0200

    Cleanup. Breaking things

--- plugins/net.bioclipse.igv/src/net/bioclipse/igv/editors/IgvEditor.java
+++ plugins/net.bioclipse.igv/src/net/bioclipse/igv/editors/IgvEditor.java
@@ -2,6 +2,9 @@ package net.bioclipse.igv.editors;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IEditorInput;
@@ -11,9 +14,48 @@ import org.eclipse.ui.part.EditorPart;
 
 public class IgvEditor extends EditorPart {
       private Label contents;
+
       public void createPartControl(Composite parent) {
-         contents = new Label(parent, SWT.NONE);
-         contents.setText("IGV Editor");
+          /*
+           * Set a Windows specific AWT property that prevents heavyweight
+           * components from erasing their background. Note that this is a global
+           * property and cannot be scoped. It might not be suitable for your
+           * application.
+           */
+          try {
+          	System.setProperty("sun.awt.noerasebackground", "true");
+          } catch (NoSuchMethodError error) {
+          }
+
+          //Set the layout for parent
+          GridLayout layout = new GridLayout();
+          layout.numColumns = 1;
+          layout.verticalSpacing = 2;
+          layout.marginWidth = 0;
+          layout.marginHeight = 2;
+          parent.setLayout(layout);
+
+          GridData layoutData = new GridData();
+          layoutData.grabExcessHorizontalSpace = true;
+          layoutData.grabExcessVerticalSpace = true;
+          parent.setLayoutData(layoutData);
+
+          //Add the Jmol composite to the top
+          Composite composite = new Composite( parent, SWT.NO_BACKGROUND 
+                                                     | SWT.EMBEDDED
+                                                     | SWT.DOUBLE_BUFFERED );
+          layout = new GridLayout();
+          composite.setLayout(layout);
+          layoutData = new GridData(GridData.FILL_BOTH);
+          composite.setLayoutData(layoutData);
+
+
+
+          java.awt.Frame awtFrame = SWT_AWT.new_Frame(composite);
+          
+          
+          
+          awtFrame.add(awtPanel);
       }
       public void init(IEditorSite site, IEditorInput input) {
          setSite(site);
