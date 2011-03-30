d9145d3625046ef9662e2921fe3f439ae186e3d0
Samuel Lampa <samuel.lampa@rilnet.com>
2011-03-30 20:31:45 +0200

    Editor is showing up, but not the IGV one (crashes)

--- plugins/net.bioclipse.igv/src/net/bioclipse/igv/editors/IgvEditor.java
+++ plugins/net.bioclipse.igv/src/net/bioclipse/igv/editors/IgvEditor.java
@@ -1,5 +1,6 @@
 package net.bioclipse.igv.editors;
 
+import org.broad.igv.ui.IGVMainFrame;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
@@ -53,9 +54,12 @@ public class IgvEditor extends EditorPart {
 
           java.awt.Frame awtFrame = SWT_AWT.new_Frame(composite);
           
+          com.jidesoft.utils.Lm.verifyLicense("The Broad Institute, MIT", "Gene Pattern",
+          "D.DQSR7z9m6fxL1IqWZ6svQFmE6vj3Q");
+
+          IGVMainFrame igvMainFrame = IGVMainFrame.getInstance();
           
-          
-          awtFrame.add(awtPanel);
+          awtFrame.add(igvMainFrame);
       }
       public void init(IEditorSite site, IEditorInput input) {
          setSite(site);
