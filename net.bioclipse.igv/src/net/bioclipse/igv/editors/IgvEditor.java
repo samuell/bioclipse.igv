5b97a994d259d65a8827ee9c1211adb3101b4777
Samuel Lampa <samuel.lampa@rilnet.com>
2011-03-30 18:10:14 +0200

    igv.product launch file now works

---
+++ net.bioclipse.igv/src/net/bioclipse/igv/editors/IgvEditor.java
@@ -0,0 +1,47 @@
+package net.bioclipse.igv.editors;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.part.EditorPart;
+
+public class IgvEditor extends EditorPart {
+      private Label contents;
+      public void createPartControl(Composite parent) {
+         contents = new Label(parent, SWT.NONE);
+         contents.setText("IGV Editor");
+      }
+      public void init(IEditorSite site, IEditorInput input) {
+         setSite(site);
+         setInput(input);
+      }
+      public void setFocus() {
+         if (this.contents != null)
+        	 this.contents.setFocus();
+      }
+      
+	@Override
+	public void doSave(IProgressMonitor arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void doSaveAs() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean isDirty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isSaveAsAllowed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+   }
