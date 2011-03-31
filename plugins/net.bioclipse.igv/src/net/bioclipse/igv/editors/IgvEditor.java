e4d9d1643029f2160d2dd5cd6291a1f5c1e97f83
Samuel Lampa <samuel.lampa@rilnet.com>
2011-03-31 14:58:48 +0200

    Got IGV showing up inside an editor now, but it also creates it's own window

--- plugins/net.bioclipse.igv/src/net/bioclipse/igv/editors/IgvEditor.java
+++ plugins/net.bioclipse.igv/src/net/bioclipse/igv/editors/IgvEditor.java
@@ -1,7 +1,15 @@
 package net.bioclipse.igv.editors;
 
+import javax.swing.JRootPane;
+
+import org.broad.igv.ui.IGVMainFrame;
+import org.broad.igv.ui.panel.IGVPanel;
+import org.broad.igv.ui.panel.MainPanel;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.awt.SWT_AWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IEditorInput;
@@ -12,8 +20,53 @@ public class IgvEditor extends EditorPart {
     private Label contents;
     
     public void createPartControl(Composite parent) {
-       contents = new Label(parent, SWT.NONE);
-       contents.setText("Minimal Editor");
+//       contents = new Label(parent, SWT.NONE);
+//       contents.setText("Minimal Editor");
+        /*
+         * Set a Windows specific AWT property that prevents heavyweight
+         * components from erasing their background. Note that this is a global
+         * property and cannot be scoped. It might not be suitable for your
+         * application.
+         */
+        try {
+        	System.setProperty("sun.awt.noerasebackground", "true");
+        } catch (NoSuchMethodError error) {
+        }
+
+        //Set the layout for parent
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 1;
+        layout.verticalSpacing = 2;
+        layout.marginWidth = 0;
+        layout.marginHeight = 2;
+        parent.setLayout(layout);
+
+        GridData layoutData = new GridData();
+        layoutData.grabExcessHorizontalSpace = true;
+        layoutData.grabExcessVerticalSpace = true;
+        parent.setLayoutData(layoutData);
+
+        //Add the Jmol composite to the top
+        Composite composite = new Composite( parent, SWT.NO_BACKGROUND 
+                                                   | SWT.EMBEDDED
+                                                   | SWT.DOUBLE_BUFFERED );
+        layout = new GridLayout();
+        composite.setLayout(layout);
+        layoutData = new GridData(GridData.FILL_BOTH);
+        composite.setLayoutData(layoutData);
+
+        java.awt.Frame awtFrame = SWT_AWT.new_Frame(composite);
+//        java.awt.Panel awtPanel 
+//            = new java.awt.Panel(new java.awt.BorderLayout());
+        
+        IGVMainFrame igvMainFrame = IGVMainFrame.getInstance();
+        JRootPane igvRootPane = igvMainFrame.getRootPane();
+
+        
+        String[] args = {};
+        igvMainFrame.startUp(args);
+        
+        awtFrame.add(igvRootPane);
     }
     public void init(IEditorSite site, IEditorInput input) {
        setSite(site);
